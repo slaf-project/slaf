f2e4651f3e77d36a6ed9974e4b75feaa8d812421
Pavan Ramkumar <pavan.ramkumar@system1.bio>
2025-07-12 12:57:27 -0700

    fix version error

--- slaf/__init__.py
+++ slaf/__init__.py
@@ -1,3 +1,10 @@
+try:
+    from importlib.metadata import version
+
+    __version__ = version("slaf")
+except ImportError:
+    __version__ = "unknown"
+
 from slaf.core import SLAFArray
 from slaf.data import SLAFConverter
 from slaf.integrations import LazyAnnData, LazyExpressionMatrix, pp
